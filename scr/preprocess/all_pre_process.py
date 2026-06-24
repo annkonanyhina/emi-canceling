385b34ffd31f426ac45ef37df17c17797d24598b
annkonanyhina <ann.konanyhina@gmail.com>
2026-06-24 16:15:19 +0300

    Rename project structure

--- scr/preprocess/all_pre_process.py
+++ scr/preprocess/all_pre_process.py
@@ -3,7 +3,7 @@ import torch
 import numpy as np
 from scipy.signal import decimate, butter, sosfilt
 from tqdm import tqdm
-from code.preprocess.SpecDecode import DataDecoder
+from scr.preprocess.SpecDecode import DataDecoder
 import re
 
 RANGE_CODE_TO_V = {
